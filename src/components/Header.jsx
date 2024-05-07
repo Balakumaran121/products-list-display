8fbb146ee59122ab9c43a47d11df6ebe51cc6fb4
Balakumaran <bkumaran121@gmail.com>
2024-05-07 21:44:03 +0530

    Add Header and Hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
 const Header = () => {
   return (
-    <div>
-      <p>Ania Design Studio</p>
+    <div className="py-3  text-rose-500 flex justify-between items-center">
+      <p className="font-semibold mx-2">Ania Design Studio</p>
       <nav>
-        <li>Products</li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <ul className="inline-flex space-x-4 mx-6">
+          <li className="underline">Products</li>
+          <li>Find Dier</li>
+          <li> Exhibitions</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
       </nav>
+      <div className="inline-flex space-x-2 mx-3 items-center">
+        <p>Cart</p>
+        <span className="bg-rose-500 text-white rounded-full h-6 w-6 flex items-center justify-center">
+          0
+        </span>
+      </div>
     </div>
   );
 };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <p className="text-[100px] font-medium py-8 text-rose-500  text-center uppercase font-serif">
+      Products
+    </p>
+  );
+};
+
+export default Hero;
